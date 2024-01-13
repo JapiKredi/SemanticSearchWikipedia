5b84d043287c1e964d6024dd240801eba7d1e601
JapiKredi <jasper.bongers@yahoo.com>
2024-01-13 20:32:27 +0800

    Actually generating the embeddings for the section chunking method

--- method3_sections_chunking.py
+++ method3_sections_chunking.py
@@ -102,4 +102,5 @@ def generate_embeddings(texts):
 def generate_embeddings_on_df(df):
   df['Embeddings'] = df['Chunk Text'].apply(lambda x: generate_embeddings([x])[0])
   
-  
+# Create embeddings for 'Chunk Text' column on all three dataframes
+generate_embeddings_on_df(section_chunk_df)
