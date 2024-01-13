5f2677967a37684478fab1b3191c2dcbcf1ff5d9
JapiKredi <jasper.bongers@yahoo.com>
2024-01-13 20:28:52 +0800

    Save embeddings into a dataframe

--- method1_fixed_size_chunking.py
+++ method1_fixed_size_chunking.py
@@ -74,3 +74,6 @@ print(fixed_chunk_df)
 def generate_embeddings(texts):
     embeddings = embedder.encode(texts, convert_to_tensor=True)
     return embeddings
+
+def generate_embeddings_on_df(df):
+  df['Embeddings'] = df['Chunk Text'].apply(lambda x: generate_embeddings([x])[0])
