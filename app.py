aad2a29781bb6051356531d0a010cec3ca86f397
JapiKredi <jasper.bongers@yahoo.com>
2024-01-13 17:05:20 +0800

    Commit

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import numpy as np
 import pandas as pd
 import wikipediaapi
+from functions import process_page, split_text_into_chunks
+
+# Call the Wikipedia function through a user_agent and specify the language
+# Call the Wikipedia function through a user_agent and specify the language
+string = "SemSearchDemo/2.0 (Jasper B)"
+wiki_wiki = wikipediaapi.Wikipedia(user_agent= string, language= 'en')
+
 
 # Iterating over all page titles to create the final df with individual chunks
 
@@ -12,7 +19,6 @@ for title in page_titles:
     df = process_page(title)
     if df is not None:
         all_dfs.append(df)
-
 
 fixed_chunk_df = pd.concat(all_dfs, ignore_index=True)
 fixed_chunk_df
